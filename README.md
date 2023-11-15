a1036c088d479f5fde4257486edae8a8cf746039
Marcel <40523792+marcelschreiner@users.noreply.github.com>
2023-11-15 20:45:41 +0100

    Cleanup + Added LICENSE.md + Added better desc.

--- README.md
+++ README.md
@@ -11,6 +11,19 @@
 [![Technical Debt](https://sonarcloud.io/api/project_badges/measure?project=marcelschreiner_loxone-hue-picoc&metric=sqale_index)](https://sonarcloud.io/summary/new_code?id=marcelschreiner_loxone-hue-picoc)
 
 
+A PicoC code to control Philips Hue lights from a Loxone Miniserver. Using this solution it is not necessary to rely on a thirdparty bridge to translate between the Miniserver and the Hue Bridge.
+
+Unfortunately this PicoC code can only control send commands to the Hue system. Is is however not possible to receive events from the Hue system. For this to work https would be required (which is not available in PicoC).
+Have a look at the [repo hue-to-loxone](https://github.com/marcelschreiner/hue-to-loxone) if you also want to receive events from motion sensors or hue dimmer switches.
+
+
+*THIS CODE IS A WORK IN PROGRESS! NOT YET FULLY WORKING!*
+
+
+## License
+
+This script is provided under the [MIT License](LICENSE.md). Feel free to modify and use it according to your needs.
+
 <br />
 
 [![SonarCloud](https://sonarcloud.io/images/project_badges/sonarcloud-black.svg)](https://sonarcloud.io/summary/new_code?id=marcelschreiner_loxone-hue-picoc)
--- hue_script.c
+++ hue_script.c
@@ -13,7 +13,7 @@
 #define RX_BUFFER_SIZE      (40000)
 #define RX_BLOCK_SIZE       (128)
 
-typedef enum bulb_t__
+enum bulb_t__
 {
     TYPE_SINGLE_RGB,
     TYPE_SINGLE_TUNABLE,
@@ -37,19 +37,19 @@ char *PORT       = "80";
 char *USERNAME   = "yourphilipshueapikey";
 
 // Set type of bulb
-bulbType[0]  = TYPE_SINGLE_TUNABLE;  // Decke Türe (Bad Kinder)
-bulbType[1]  = TYPE_SINGLE_TUNABLE;  // Decke Dusche (Bad Kinder)
-bulbType[2]  = TYPE_SINGLE_TUNABLE;  // Decke Mitte (Bad Kinder)
+bulbType[0]  = TYPE_SINGLE_TUNABLE;
+bulbType[1]  = TYPE_SINGLE_TUNABLE;
+bulbType[2]  = TYPE_SINGLE_TUNABLE;
 bulbType[3]  = TYPE_NONE;
 bulbType[4]  = TYPE_NONE;
-bulbType[5]  = TYPE_SINGLE_RGB;  // Spiegel links (Bad Kinder)
-bulbType[6]  = TYPE_SINGLE_RGB;  // Spiegel rechts (Bad Kinder)
+bulbType[5]  = TYPE_SINGLE_RGB;
+bulbType[6]  = TYPE_SINGLE_RGB;
 bulbType[7]  = TYPE_NONE;
 bulbType[8]  = TYPE_NONE;
 bulbType[9]  = TYPE_NONE;
-bulbType[10] = TYPE_SINGLE_TUNABLE;  // Blop Bett
-bulbType[11] = TYPE_SINGLE_TUNABLE;  // Blop Fenster
-bulbType[12] = TYPE_SINGLE_RGB;  // Ambiente (Bad Kinder)
+bulbType[10] = TYPE_SINGLE_TUNABLE;
+bulbType[11] = TYPE_SINGLE_TUNABLE;
+bulbType[12] = TYPE_SINGLE_RGB;
 
 // Set bulb or group id
 bulbId[0] = 14;
@@ -65,288 +65,36 @@ bulbId[9] = 23;
 bulbId[10] = 25;
 bulbId[11] = 26;
 bulbId[12] = 13;
+
 // ***************************************************
 //  Configure your system here                     END
 // ***************************************************
 
 // Returns 0 if single bulb, 1 if group
-int isGroup(int type)
+int IsGroup(int type)
 {
-    int type = 0;
+    int boolIsGroup = 0;
     switch(type)
     {
         case TYPE_SINGLE_RGB:
         case TYPE_SINGLE_TUNABLE:
         case TYPE_SINGLE_DIM:
         case TYPE_SINGLE_ONOFF:
-            type = 0;
+            boolIsGroup = 0;
             break;
 
         case TYPE_GROUP_RGB:
         case TYPE_GROUP_TUNABLE:
         case TYPE_GROUP_DIM:
         case TYPE_GROUP_ONOFF:
-            type = 1;
+            boolIsGroup = 1;
             break;
     }
-    return type;
+    return boolIsGroup;
 }
 
 
-void updateLamp(int i, int value)
-{
-    switch (bulbType[i])
-    {
-        case TYPE_SINGLE_RGB:
-        case TYPE_GROUP_RGB:
-            if (value < 200000000)
-            {
-                setRGB(bulbId[i], value, isGroup(bulbType[i]));
-            }
-            else
-            {
-                setTunable(bulbId[i], value, isGroup(bulbType[i]));
-            }
-            break;
-
-        case TYPE_SINGLE_TUNABLE:
-        case TYPE_GROUP_TUNABLE:
-            if ((200000000 <= value) || (0 == value))
-            {
-                setTunable(bulbId[i], value, isGroup(bulbType[i]));
-            }
-            break;
-
-        case TYPE_SINGLE_DIM:
-        case TYPE_GROUP_DIM:
-            setDim(bulbId[i], value, isGroup(bulbType[i]));
-            break;
-
-        case TYPE_SINGLE_ONOFF:
-        case TYPE_GROUP_ONOFF:
-            setOnOff(bulbId[i], value, isGroup(bulbType[i]));
-            break;
-    }
-}
-
-
-void setOnOff(int bulbId, int state, int type)
-{
-    char command[50];
-
-    if (0 == state)
-    {
-        sprintf(command, "{\"on\": false, \"transitiontime\": 10}");
-    }
-    else
-    {
-        sprintf(command, "{\"on\": true, \"transitiontime\": 10}");
-    }
-    sendCommand(type, bulbId, command);
-}
-
-
-void setDim(int bulbId, int brightness, int type)
-{
-    char command[60];
-
-    // Check if brightness is set to 0 and leave early
-    if (0 == brightness)
-    {
-        setOnOff(bulbId, 0, type);
-        return;
-    } 
-
-    // Change range of brightness 1-100 -> 1-255
-    brightness = (brightness * 255) / 100;
-
-    sprintf(command, "{\"on\": true, \"bri\": %d, \"transitiontime\": 10}", brightness);
-    sendCommand(type, bulbId, command);
-}
-
-
-void setTunable(int bulbId, int value, int type)
-{
-    char command[100];
-    int brightness;
-    int temperature;
-
-    // Check if brightness is set to 0 and leave early
-    if (0 == brightness)
-    {
-        setOnOff(bulbId, 0, type);
-        return;
-    }
-
-    brightness  = (value - 200000000) / 10000;                  // 0-100
-    temperature = (value - 200000000) - (brightness * 10000);   // Kelvin 2700 - 6500
-
-    brightness  = (brightness * 255) / 100;                     // 0-255
-    temperature = 1000000 / temperature;                        // 154 - 370
-
-    sprintf(command, "{\"on\": true, \"bri\": %d, \"ct\": %d, \"transitiontime\": 10}", brightness, temperature);
-    sendCommand(type, bulbId, command);
-}
-
-
-void setRGB(int bulbId, int value, int type)
-{
-    int red;
-    int green;
-    int blue;
-
-    // Check if brightness is set to 0 and leave early
-    if (0 == value)
-    {
-        setOnOff(bulbId, 0, type);
-        return;
-    }
-
-    blue  = value / 1000000;
-    green = (value - (blue * 1000000)) / 1000;
-    red   = value - (blue * 1000000) - (green * 1000);
-
-    // Use ether HSB or XYB to set RGB colors (Choose whatever works best for you!)
-    setColorXYB(bulbId, (float)red, (float)green, (float)blue, type);
-    //setColorHSB(bulbId, (float)red, (float)green, (float)blue, type);
-}
-
-
-void setColorXYB(int bulbId, float red, float green, float blue, int type)
-{
-    float cx, cy, bri;
-    float X, Y, Z;
-    char command[100];
-
-    bri = blue;
-    if (bri < green)
-        bri = green;
-    if (bri < red)
-        bri = red;
-    bri = bri * 2.55;
-
-    blue = blue / 100;
-    green = green / 100;
-    red = red / 100;
-
-    // Apply gamma correction
-    if (red > 0.04045)
-    {
-        red = pow((red + 0.055) / 1.055, 2.4);
-    }
-    else
-    {
-        red = red / 12.92;
-    }
-    if (green > 0.04045)
-    {
-        green = pow((green + 0.055) / 1.055, 2.4);
-    }
-    else
-    {
-        green = green / 12.92;
-    }
-    if (blue > 0.04045)
-    {
-        blue = pow((blue + 0.055) / 1.055, 2.4);
-    }
-    else
-    {
-        blue = blue / 12.92;
-    }
-
-    // Wide gamut conversion D65
-    X = red * 0.664511f + green * 0.154324f + blue * 0.162028f;
-    Y = red * 0.283881f + green * 0.668433f + blue * 0.047685f;
-    Z = red * 0.000088f + green * 0.072310f + blue * 0.986039f;
-
-    // Calculate xy and bri
-    if ((X + Y + Z) == 0)
-    {
-        cx = 0;
-        cy = 0;
-    }
-    else
-    { // round to 4 decimal max (=api max size)
-        cx = X / (X + Y + Z);
-        cy = Y / (X + Y + Z);
-    }
-
-    sprintf(command, "{\"xy\": [%f,%f],\"bri\": %d,\"on\":true, \"transitiontime\": 10}", cx, cy, bri);
-
-    sendCommand(type, bulbId, command);
-}
-
-
-// nochmal umrechnen nach hue irgendwie, weil die Living Colors Gen2 irgendwie nich gehen mit xy
-void setColorHSB(int bulbId, float red, float green, float blue, int type)
-{
-    float hue, sat, bri;
-    char command[100];
-
-    hue = 0;
-    sat = 0;
-    bri = 0;
-
-    if (blue > 0 || green > 0 || red > 0)
-    {
-        if ((red >= green) && (green >= blue))
-        {
-            if (red == blue)
-            {
-                hue = 0;
-            }
-            else
-            {
-                hue = 60 * (green - blue) / (red - blue);
-            }
-            sat = (red - blue) / red;
-            bri = red;
-        }
-        else if ((green > red) && (red >= blue))
-        {
-            hue = 60 * (2 - (red - blue) / (green - blue));
-            sat = (green - blue) / green;
-            bri = green;
-        }
-        else if ((green >= blue) && (blue > red))
-        {
-            hue = 60 * (2 + (blue - red) / (green - red));
-            sat = (green - red) / green;
-            bri = green;
-        }
-        else if ((blue > green) && (green > red))
-        {
-            hue = 60 * (4 - (green - red) / (blue - red));
-            sat = (blue - red) / blue;
-            bri = blue;
-        }
-        else if ((blue > red) && (red >= green))
-        {
-            hue = 60 * (4 + (red - green) / (blue - green));
-            sat = (blue - green) / blue;
-            bri = blue;
-        }
-        else if ((red >= blue) && (blue > green))
-        {
-            hue = 60 * (6 - (blue - green) / (red - green));
-            sat = (red - green) / red;
-            bri = red;
-        }
-
-        // hue = 0-65535, sat 0-255, bri 0-255
-        hue = hue / 360 * 65535;
-        sat = sat * 255;
-        bri = bri * 2.55;
-    }
-
-    sprintf(command, "{\"bri\": %d, \"hue\": %d, \"sat\": %d, \"on\": true, \"transitiontime\": 10}", (int)bri, (int)hue, (int)sat);
-
-    sendCommand(type, bulbId, command);
-}
-
-
-void sendCommand(int type, int bulbId, char *command)
+void SendCommand(int type, int bulbId, char *command)
 {
     char streamname[100];
     char selector[30];
@@ -413,24 +161,287 @@ void sendCommand(int type, int bulbId, char *command)
 }
 
 
-// Main application
-int inputsThatChanged;
-int i;
-
-while (1)
+void SetOnOff(int bulbId, int state, int type)
 {
-    // Get a bitmask of the changed inputs (bit 0 = first input of object, starts with text inputs followed by analog inputs).
-    inputsThatChanged = getinputevent();
+    char command[50];
 
-    // Loop from AI1 to AI13 and update lamp if the corresponding input changed
-    for (i = 0; i < 13; i++)
+    if (0 == state)
     {
-        if (inputsThatChanged & (0x8 << i))
-        {
-            updateLamp(i, (int)getinput(i));
-            sleep(100);
-        }
+        sprintf(command, "{\"on\": false, \"transitiontime\": 10}");
+    }
+    else
+    {
+        sprintf(command, "{\"on\": true, \"transitiontime\": 10}");
+    }
+    SendCommand(type, bulbId, command);
+}
+
+
+void SetDim(int bulbId, int brightness, int type)
+{
+    char command[60];
+
+    // Check if brightness is set to 0 and leave early
+    if (0 == brightness)
+    {
+        SetOnOff(bulbId, 0, type);
+        return;
+    } 
+
+    // Change range of brightness 1-100 -> 1-255
+    brightness = (brightness * 255) / 100;
+
+    sprintf(command, "{\"on\": true, \"bri\": %d, \"transitiontime\": 10}", brightness);
+    SendCommand(type, bulbId, command);
+}
+
+
+void SetTunable(int bulbId, int value, int type)
+{
+    char command[100];
+    int brightness;
+    int temperature;
+
+    // Check if brightness is set to 0 and leave early
+    if (0 == brightness)
+    {
+        SetOnOff(bulbId, 0, type);
+        return;
     }
 
-    sleep(100);
+    brightness  = (value - 200000000) / 10000;                  // 0-100
+    temperature = (value - 200000000) - (brightness * 10000);   // Kelvin 2700 - 6500
+
+    brightness  = (brightness * 255) / 100;                     // 0-255
+    temperature = 1000000 / temperature;                        // 154 - 370
+
+    sprintf(command, "{\"on\": true, \"bri\": %d, \"ct\": %d, \"transitiontime\": 10}", brightness, temperature);
+    SendCommand(type, bulbId, command);
 }
+
+
+void SetColorXYB(int bulbId, float red, float green, float blue, int type)
+{
+    float cx, cy, bri;
+    float X, Y, Z;
+    char command[100];
+
+    bri = blue;
+    if (bri < green)
+    {
+        bri = green;
+    }
+    if (bri < red)
+    {
+        bri = red;
+    }
+    bri = bri * 2.55;
+
+    blue = blue / 100;
+    green = green / 100;
+    red = red / 100;
+
+    // Apply gamma correction
+    if (red > 0.04045)
+    {
+        red = pow((red + 0.055) / 1.055, 2.4);
+    }
+    else
+    {
+        red = red / 12.92;
+    }
+    if (green > 0.04045)
+    {
+        green = pow((green + 0.055) / 1.055, 2.4);
+    }
+    else
+    {
+        green = green / 12.92;
+    }
+    if (blue > 0.04045)
+    {
+        blue = pow((blue + 0.055) / 1.055, 2.4);
+    }
+    else
+    {
+        blue = blue / 12.92;
+    }
+
+    // Wide gamut conversion D65
+    X = red * 0.664511f + green * 0.154324f + blue * 0.162028f;
+    Y = red * 0.283881f + green * 0.668433f + blue * 0.047685f;
+    Z = red * 0.000088f + green * 0.072310f + blue * 0.986039f;
+
+    // Calculate xy and bri
+    if ((X + Y + Z) == 0)
+    {
+        cx = 0;
+        cy = 0;
+    }
+    else
+    { // round to 4 decimal max (=api max size)
+        cx = X / (X + Y + Z);
+        cy = Y / (X + Y + Z);
+    }
+
+    sprintf(command, "{\"xy\": [%f,%f],\"bri\": %d,\"on\":true, \"transitiontime\": 10}", cx, cy, bri);
+
+    SendCommand(type, bulbId, command);
+}
+
+
+// Living Colors Gen2 does not work with xy
+void SetColorHSB(int bulbId, float red, float green, float blue, int type)
+{
+    float hue, sat, bri;
+    char command[100];
+
+    hue = 0;
+    sat = 0;
+    bri = 0;
+
+    if (blue > 0 || green > 0 || red > 0)
+    {
+        if ((red >= green) && (green >= blue))
+        {
+            if (red == blue)
+            {
+                hue = 0;
+            }
+            else
+            {
+                hue = 60 * (green - blue) / (red - blue);
+            }
+            sat = (red - blue) / red;
+            bri = red;
+        }
+        else if ((green > red) && (red >= blue))
+        {
+            hue = 60 * (2 - (red - blue) / (green - blue));
+            sat = (green - blue) / green;
+            bri = green;
+        }
+        else if ((green >= blue) && (blue > red))
+        {
+            hue = 60 * (2 + (blue - red) / (green - red));
+            sat = (green - red) / green;
+            bri = green;
+        }
+        else if ((blue > green) && (green > red))
+        {
+            hue = 60 * (4 - (green - red) / (blue - red));
+            sat = (blue - red) / blue;
+            bri = blue;
+        }
+        else if ((blue > red) && (red >= green))
+        {
+            hue = 60 * (4 + (red - green) / (blue - green));
+            sat = (blue - green) / blue;
+            bri = blue;
+        }
+        else if ((red >= blue) && (blue > green))
+        {
+            hue = 60 * (6 - (blue - green) / (red - green));
+            sat = (red - green) / red;
+            bri = red;
+        }
+
+        // hue = 0-65535, sat 0-255, bri 0-255
+        hue = hue / 360 * 65535;
+        sat = sat * 255;
+        bri = bri * 2.55;
+    }
+
+    sprintf(command, "{\"bri\": %d, \"hue\": %d, \"sat\": %d, \"on\": true, \"transitiontime\": 10}", (int)bri, (int)hue, (int)sat);
+
+    SendCommand(type, bulbId, command);
+}
+
+
+void SetRGB(int bulbId, int value, int type)
+{
+    int red;
+    int green;
+    int blue;
+
+    // Check if brightness is set to 0 and leave early
+    if (0 == value)
+    {
+        SetOnOff(bulbId, 0, type);
+        return;
+    }
+
+    blue  = value / 1000000;
+    green = (value - (blue * 1000000)) / 1000;
+    red   = value - (blue * 1000000) - (green * 1000);
+
+    // Use ether HSB or XYB to set RGB colors (Choose whatever works best for you!)
+    SetColorXYB(bulbId, (float)red, (float)green, (float)blue, type);
+    //SetColorHSB(bulbId, (float)red, (float)green, (float)blue, type);
+}
+
+
+void UpdateLamp(int i, int value)
+{
+    switch (bulbType[i])
+    {
+        case TYPE_SINGLE_RGB:
+        case TYPE_GROUP_RGB:
+            if (value < 200000000)
+            {
+                SetRGB(bulbId[i], value, IsGroup(bulbType[i]));
+            }
+            else
+            {
+                SetTunable(bulbId[i], value, IsGroup(bulbType[i]));
+            }
+            break;
+
+        case TYPE_SINGLE_TUNABLE:
+        case TYPE_GROUP_TUNABLE:
+            if ((200000000 <= value) || (0 == value))
+            {
+                SetTunable(bulbId[i], value, IsGroup(bulbType[i]));
+            }
+            break;
+
+        case TYPE_SINGLE_DIM:
+        case TYPE_GROUP_DIM:
+            SetDim(bulbId[i], value, IsGroup(bulbType[i]));
+            break;
+
+        case TYPE_SINGLE_ONOFF:
+        case TYPE_GROUP_ONOFF:
+            SetOnOff(bulbId[i], value, IsGroup(bulbType[i]));
+            break;
+    }
+}
+
+
+int main(void)
+{
+    int inputsThatChanged;
+    int i;
+
+    while (1)
+    {
+        // Get a bitmask of the changed inputs (bit 0 = first input of object, starts with text inputs followed by analog inputs).
+        inputsThatChanged = 0;//getinputevent();
+
+        // Loop from AI1 to AI13 and update lamp if the corresponding input changed
+        for (i = 0; i < 13; i++)
+        {
+            if (inputsThatChanged & (0x8 << i))
+            {
+                UpdateLamp(i, /*(int)getinput(i)*/0);
+                sleep(100);
+            }
+        }
+
+        sleep(100);
+    }
+    return 0;
+}
+
+
+main();
